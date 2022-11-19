5f821a82ae85fe833501a0d4f622db94ca84751d
Лилия Калимуллина <lilyasafi2011@mail.ru>
2022-11-19 22:28:48 +0500

    Code stile Practica, added Docstring

--- temp.py
+++ temp.py
@@ -1,42 +1,22 @@
-# Тестовые данные.
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+print(message)
 
 
-TEST_DATA: list[tuple[int, str, bool]] = [
-    (44, 'success', True),
-    (16, 'failure', True),
-    (4, 'success', False),
-    (21, 'failure', False),
-]
-
-BONUS: float = 1.1
-ANTIBONUS: float = 0.8
+def calculateSquareRoot(Number: float) -> float:
+    """Вычисляет квадратный корень."""
+    return sqrt(Number)
 
 
-def add_rep(current_rep: float, rep_points: int, buf_effect: bool) -> float:
-    current_rep += rep_points
-    if buf_effect:
-        return current_rep * BONUS
-    return current_rep
+def calc(your_number: float) -> float:
+    """Проверяет на отрицательные значения."""
+    if your_number <= 0:
+        return 0
+    print(f'Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет:{calculateSquareRoot(your_number)}')
 
 
-def remove_rep(current_rep: float, rep_points: int,
-               debuf_effect: bool) -> float:
-    current_rep -= rep_points
-    if debuf_effect:
-        return current_rep * ANTIBONUS
-    return current_rep
-
-
-def main(duel_res: list[tuple[int, str, bool]]) -> str:
-    current_rep: float = 0.0
-    for rep, result, effect in duel_res:
-        if result == 'success':
-            current_rep = add_rep(current_rep, rep, effect)
-        if result == 'failure':
-            current_rep = remove_rep(current_rep, rep, effect)
-    return (f'После {len(duel_res)} поединков, репутация персонажа '
-            f'— {current_rep:.3f} очков.')
-
-
-# Тестовый вызов функции main.
-print(main(TEST_DATA))
+calc(25.5)
